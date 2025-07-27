1a4ec767bb73d33dafc06f5b38649ac22b03c281
Fathur Rizqi <fathurrizqizdjafar@gmail.com>
2025-07-27 21:20:28 +0800

    tes

--- express/utils/db.js
+++ express/utils/db.js
@@ -1,12 +1,23 @@
 const mysql = require('mysql2/promise');
 
+// const pool = mysql.createPool({
+//   host: 'localhost',     // ubah ke host db kamu
+//   user: 'root',
+//   password: '',
+//   database: 'apikasirdev',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
 const pool = mysql.createPool({
-  host: 'localhost',     // ubah ke host db kamu
-  user: 'root',
-  password: '',
-  database: 'apikasirdev',
+  host: process.env.DB_HOST,     // dari .env atau Environment Variables di Vercel
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool;
+
